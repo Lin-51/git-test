97ad78223dbec7496a2f81463b4faf21b1a82fc7
lin <lin@qq.com>
2022-04-25 18:50:37 +0800

    最新版

--- src/com/demo.java
+++ src/com/demo.java
@@ -4,6 +4,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello,word");
         System.out.println("A2004");
+        System.out.println("最新代码");
     }
 
 }

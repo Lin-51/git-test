c4dc72ed71a74bb7c31bcb4d6292272b22bb3d8e
lin <lin@qq.com>
2022-04-25 18:22:32 +0800

    修改两行代码

--- src/com/demo4.java
+++ src/com/demo4.java
@@ -5,5 +5,7 @@ public class demo4 {
     public static void main(String[] args) {
         System.out.println("demo4bug");
         System.out.println("测试git");
+        System.out.println("测试11111");
+
     }
 }

6e0c5a332cfd7076659f38c40cfaa9d4f291fccf
lin <lin@qq.com>
2022-04-25 18:44:49 +0800

    A2004秀改

--- src/com/demo1.java
+++ src/com/demo1.java
@@ -5,5 +5,7 @@ public class demo1 {
         System.out.println("demo");
         System.out.println("修改问题");
         System.out.println("我是bug分之");
+        System.out.println("ssss");
+        System.out.println("今天很开心");
     }
 }
